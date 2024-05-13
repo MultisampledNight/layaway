9302b437785eb6bf6c0f5a732757b3f387f380d0
MultisampledNight <contact@multisamplednight.com>
2024-05-13 15:24:36 +0200

    feat: allow iteration over outputs

--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,32 @@ pub fn run() -> Result<()> {
 
 /// Where each [`Output`] concretely is, pixel-wise.
 pub struct ConcreteLayout {
-    pub outputs: BTreeMap<Port, Rect>,
+    pub outputs: BTreeMap<Port, OutputConfig>,
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Output {
-    port: Port,
+impl ConcreteLayout {
+    pub fn outputs(&self) -> impl Iterator<Item = Output<'_>> {
+        self.outputs.iter().map(|(port, cfg)| Output { port, cfg })
+    }
+}
+
+/// Something that Sway can display to. Usually a screen.
+#[derive(Copy, Clone, PartialEq, PartialOrd)]
+pub struct Output<'layout> {
+    /// Where this output is physically connected.
+    port: &'layout Port,
+    /// Properties of this output, like size and scale.
+    cfg: &'layout OutputConfig,
+}
+
+/// Configuration for a given output in sway.
+#[derive(Copy, Clone, PartialEq, PartialOrd)]
+pub struct OutputConfig {
+    /// Where this output is placed in Sway.
     dims: Rect,
+    /// With what size multiplier to have Wayland applications rendered.
+    /// if they are visible on this output.
+    scale: f64,
 }
 
 /// Where an [`Output`] is plugged in.
